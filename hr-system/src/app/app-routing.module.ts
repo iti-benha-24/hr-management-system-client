eb26be0c3b1a3540f6d90cdd654bb3498d77b7e9
Shrouk Matar <shroukk.matar@gmail.com>
2024-03-24 00:01:46 +0200

    login with authentication

--- hr-system/src/app/app-routing.module.ts
+++ hr-system/src/app/app-routing.module.ts
@@ -12,17 +12,17 @@ import { LoginComponent } from './components/login/login.component';
 import { PublicHolidayInsertComponent } from './components/public-holiday-insert/public-holiday-insert.component';
 const routes: Routes = [
   // Add routes here
-  {path: 'home', component: AttendanceComponent , canActivate:[AuthGuardService]},
-  {path: 'register', component: RegistrationComponent },
-  {path: 'login', component: LoginComponent},
-  {path: 'attendance',component: AttendanceComponent , canActivate:[AuthGuardService]},
-  {path:'salaryReport',component:SalaryReportComponent , canActivate:[AuthGuardService] },
-  {path:'add',component:AttendanceFormComponent , canActivate:[AuthGuardService] },
-  {path:'edit/:id',component:AttendanceFormComponent , canActivate:[AuthGuardService]},
-  { path : "holidays", component : PublicHolidayGetAllComponent , canActivate:[AuthGuardService] },
-  { path : "insert" , component : PublicHolidayInsertComponent  , canActivate:[AuthGuardService] },
-  { path : 'update' , component : PublicHolidayUpdateComponent , canActivate:[AuthGuardService] }
-
+  { path: 'home', component: AttendanceComponent, canActivate: [AuthGuardService] },
+  { path: 'register', component: RegistrationComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'attendance', component: AttendanceComponent, canActivate: [AuthGuardService] },
+  { path: 'salaryReport', component: SalaryReportComponent, canActivate: [AuthGuardService] },
+  { path: 'add', component: AttendanceFormComponent, canActivate: [AuthGuardService] },
+  { path: 'edit/:id', component: AttendanceFormComponent, canActivate: [AuthGuardService] },
+  { path: "holidays", component: PublicHolidayGetAllComponent, canActivate: [AuthGuardService] },
+  { path: "insert", component: PublicHolidayInsertComponent, canActivate: [AuthGuardService] },
+  { path: 'update', component: PublicHolidayUpdateComponent, canActivate: [AuthGuardService] },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
 @NgModule({
