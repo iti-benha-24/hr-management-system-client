23c67ab689ef1e152d8004c2ad64494ed47267ca
hussein <husseinsaid571@gmail.com>
2024-04-18 00:55:44 +0200

    updates

--- hr-system/src/app/app-routing.module.ts
+++ hr-system/src/app/app-routing.module.ts
@@ -1,3 +1,77 @@
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// // import { AttendanceComponent } from './Components/attendance/attendance.component';
+// import { AttendanceFormComponent } from './Components/attendance-form/attendance-form.component';
+// import { SalaryReportComponent } from './Components/salary-report/salary-report.component';
+// import { RegistrationComponent } from './Components/registration/registration.component';
+// import { AuthGuardService } from 'src/services/auth-guard.service';
+// import { PublicHolidayGetAllComponent } from './Components/public-holiday-get-all/public-holiday-get-all.component';
+// import { LoginComponent } from './Components/login/login.component';
+// import { PublicHolidayInsertComponent } from './Components/public-holiday-insert/public-holiday-insert.component';
+// import { EmployeeComponent } from './Components/employee/employee.component';
+// import { AddEmployeeComponent } from './Components/add-employee/add-employee.component';
+// import { HomeComponent } from './Components/home/home.component';
+// import { GeneralSettingComponent } from './Components/general-setting/general-setting.component';
+// import { PermissionsComponent } from './Components/permissions/permissions.component';
+// import { PermissionFormComponent } from './Components/permission-form/permission-form.component';
+// import { PublicHolidayGetAllComponent } from './components/public-holiday-get-all/public-holiday-get-all.component';
+// import { LoginComponent } from './components/login/login.component';
+// import { PublicHolidayInsertComponent } from './components/public-holiday-insert/public-holiday-insert.component';
+// import { EmployeeComponent } from './components/employee/employee.component';
+// import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+// import { HomeComponent } from './components/home/home.component';
+// import { GeneralSettingComponent } from './components/general-setting/general-setting.component';
+// import { PermissionsComponent } from './components/permissions/permissions.component';
+// import { PermissionFormComponent } from './components/permission-form/permission-form.component';
+// import { DepartmentComponent } from './components/department/department.component';
+// import { DepartmentFormComponent } from './components/department-form/department-form.component';
+// import { UsersComponent } from './components/users/users.component';
+// import { AttendanceComponent } from './Components/attendance/attendance.component';
+
+
+
+
+
+// const routes: Routes = [
+//   // Add routes here
+
+  
+//   { path: 'home', component:HomeComponent , canActivate: [AuthGuardService] },
+//   { path: 'register', component: RegistrationComponent , canActivate: [AuthGuardService] },
+//   { path: 'login', component: LoginComponent },
+//   { path: 'users', component: UsersComponent, canActivate: [AuthGuardService] },
+//   { path: 'attendance', component: AttendanceComponent, canActivate: [AuthGuardService] },
+//   { path: 'salaryReport', component: SalaryReportComponent, canActivate: [AuthGuardService] },
+//   { path: 'add', component: AttendanceFormComponent, canActivate: [AuthGuardService] },
+//   { path: 'edit/:id', component: AttendanceFormComponent, canActivate: [AuthGuardService] },
+//   { path: "holidays", component: PublicHolidayGetAllComponent, canActivate: [AuthGuardService] },
+//   { path: "insert", component: PublicHolidayInsertComponent, canActivate: [AuthGuardService] },
+//   { path: 'editholiday/:id', component: PublicHolidayInsertComponent, canActivate: [AuthGuardService] },
+ 
+
+//   {path:'department',component:DepartmentComponent, canActivate: [AuthGuardService] },
+//   {path:'addDept',component:DepartmentFormComponent, canActivate: [AuthGuardService] },
+//   {path:'editDept/:id',component:DepartmentFormComponent, canActivate: [AuthGuardService] },
+
+//   {path:'employee',component:EmployeeComponent, canActivate: [AuthGuardService] },
+//   { path:'addEmployee',component:AddEmployeeComponent, canActivate: [AuthGuardService] },
+//   {path:'addEmployee/:id',component:AddEmployeeComponent, canActivate: [AuthGuardService] },
+//   {path:'settings/:id',component:GeneralSettingComponent, canActivate: [AuthGuardService] },
+//   {path:'permissions',component:PermissionsComponent, canActivate: [AuthGuardService] },
+//   {path:'newrole',component:PermissionFormComponent, canActivate: [AuthGuardService] },
+//   {path:'editrole/:id',component:PermissionFormComponent, canActivate: [AuthGuardService] },
+
+//    { path: '', redirectTo: '/home', pathMatch: 'full' },
+// ];
+
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
+
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -17,24 +91,17 @@ import { HomeComponent } from './Components/home/home.component';
 import { GeneralSettingComponent } from './Components/general-setting/general-setting.component';
 import { PermissionsComponent } from './Components/permissions/permissions.component';
 import { PermissionFormComponent } from './Components/permission-form/permission-form.component';
-import { PublicHolidayGetAllComponent } from './components/public-holiday-get-all/public-holiday-get-all.component';
-import { LoginComponent } from './components/login/login.component';
-import { PublicHolidayInsertComponent } from './components/public-holiday-insert/public-holiday-insert.component';
-import { EmployeeComponent } from './components/employee/employee.component';
-import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
-import { HomeComponent } from './components/home/home.component';
-import { GeneralSettingComponent } from './components/general-setting/general-setting.component';
-import { PermissionsComponent } from './components/permissions/permissions.component';
-import { PermissionFormComponent } from './components/permission-form/permission-form.component';
-import { DepartmentComponent } from './components/department/department.component';
-import { DepartmentFormComponent } from './components/department-form/department-form.component';
-import { UsersComponent } from './components/users/users.component';
+ import { DepartmentComponent } from './components/department/department.component';
+ import { DepartmentFormComponent } from './components/department-form/department-form.component';
+ import { UsersComponent } from './components/users/users.component';
+
+
 const routes: Routes = [
   // Add routes here
 
   
   { path: 'home', component:HomeComponent , canActivate: [AuthGuardService] },
-  { path: 'register', component: RegistrationComponent , canActivate: [AuthGuardService] },
+  { path: 'register', component: RegistrationComponent },
   { path: 'login', component: LoginComponent },
   { path: 'users', component: UsersComponent, canActivate: [AuthGuardService] },
   { path: 'attendance', component: AttendanceComponent, canActivate: [AuthGuardService] },
@@ -44,8 +111,7 @@ const routes: Routes = [
   { path: "holidays", component: PublicHolidayGetAllComponent, canActivate: [AuthGuardService] },
   { path: "insert", component: PublicHolidayInsertComponent, canActivate: [AuthGuardService] },
   { path: 'editholiday/:id', component: PublicHolidayInsertComponent, canActivate: [AuthGuardService] },
- 
-
+  
   {path:'department',component:DepartmentComponent, canActivate: [AuthGuardService] },
   {path:'addDept',component:DepartmentFormComponent, canActivate: [AuthGuardService] },
   {path:'editDept/:id',component:DepartmentFormComponent, canActivate: [AuthGuardService] },
@@ -58,7 +124,7 @@ const routes: Routes = [
   {path:'newrole',component:PermissionFormComponent, canActivate: [AuthGuardService] },
   {path:'editrole/:id',component:PermissionFormComponent, canActivate: [AuthGuardService] },
 
-   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
 
 
@@ -66,4 +132,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { }
--- hr-system/src/app/app.module.ts
+++ hr-system/src/app/app.module.ts
@@ -1,3 +1,83 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { PublicHolidayGetAllComponent } from './Components/public-holiday-get-all/public-holiday-get-all.component';
+// import { PublicHolidayInsertComponent } from './Components/public-holiday-insert/public-holiday-insert.component';
+// import { SalaryReportComponent } from './Components/salary-report/salary-report.component';
+// import { HttpClient, HttpClientModule } from '@angular/common/http';
+// import { AttendanceComponent } from './Components/attendance/attendance.component';
+// import { AttendanceFormComponent } from './Components/attendance-form/attendance-form.component';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+// import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+// import { RegistrationComponent } from './components/registration/registration.component';
+// import { LoginComponent } from './components/login/login.component';
+// import { HeaderComponent } from './components/header/header.component';
+// import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+// import { EmployeeComponent } from './components/employee/employee.component';
+// import { GetEmpByIdComponent } from './components/get-emp-by-id/get-emp-by-id.component';
+// import { HomeComponent } from './components/home/home.component';
+// import { GeneralSettingComponent } from './components/general-setting/general-setting.component';
+// import { PermissionsComponent } from './components/permissions/permissions.component';
+// import { PermissionFormComponent } from './components/permission-form/permission-form.component';
+// import { DepartmentComponent } from './components/department/department.component';
+// import { DepartmentFormComponent } from './components/department-form/department-form.component';
+// import { UsersComponent } from './components/users/users.component';
+// // import { IonicModule } from '@ionic/angular';
+
+
+// export function HttpLoaderFactory(http: HttpClient) {
+//   return new TranslateHttpLoader(http);
+// }
+
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     AttendanceComponent,
+//     SalaryReportComponent,
+//     AttendanceFormComponent,
+//     RegistrationComponent,
+//     HeaderComponent,
+//     LoginComponent,
+//     PublicHolidayGetAllComponent,
+//     PublicHolidayInsertComponent,
+//     EmployeeComponent,
+//     GetEmpByIdComponent,
+//     AddEmployeeComponent,
+//     HomeComponent,
+//     GeneralSettingComponent,
+//     PermissionsComponent,
+//     PermissionFormComponent,
+//     DepartmentComponent,
+//     DepartmentFormComponent,
+//     UsersComponent,
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     HttpClientModule ,
+//     FormsModule ,
+//     ReactiveFormsModule ,
+//     TranslateModule.forRoot({
+//       loader: {
+//         provide: TranslateLoader,
+//         useFactory: HttpLoaderFactory,
+//         deps: [HttpClient]
+//       }
+//     }),
+
+//   ],
+//   providers: [HttpClient],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
+
+
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -9,29 +89,26 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AttendanceComponent } from './Components/attendance/attendance.component';
 import { AttendanceFormComponent } from './Components/attendance-form/attendance-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { RegistrationComponent } from './Components/registration/registration.component';
+import { LoginComponent } from './Components/login/login.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { AddEmployeeComponent } from './Components/add-employee/add-employee.component';
+import { EmployeeComponent } from './Components/employee/employee.component';
+import { GetEmpByIdComponent } from './Components/get-emp-by-id/get-emp-by-id.component';
+import { HomeComponent } from './Components/home/home.component';
+import { GeneralSettingComponent } from './Components/general-setting/general-setting.component';
+import { PermissionsComponent } from './Components/permissions/permissions.component';
+import { PermissionFormComponent } from './Components/permission-form/permission-form.component';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { RegistrationComponent } from './components/registration/registration.component';
-import { LoginComponent } from './components/login/login.component';
-import { HeaderComponent } from './components/header/header.component';
-import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
-import { EmployeeComponent } from './components/employee/employee.component';
-import { GetEmpByIdComponent } from './components/get-emp-by-id/get-emp-by-id.component';
-import { HomeComponent } from './components/home/home.component';
-import { GeneralSettingComponent } from './components/general-setting/general-setting.component';
-import { PermissionsComponent } from './components/permissions/permissions.component';
-import { PermissionFormComponent } from './components/permission-form/permission-form.component';
 import { DepartmentComponent } from './components/department/department.component';
 import { DepartmentFormComponent } from './components/department-form/department-form.component';
 import { UsersComponent } from './components/users/users.component';
-// import { IonicModule } from '@ionic/angular';
 
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
 }
-
 
 @NgModule({
   declarations: [
@@ -61,6 +138,10 @@ export function HttpLoaderFactory(http: HttpClient) {
     HttpClientModule ,
     FormsModule ,
     ReactiveFormsModule ,
+    AppRoutingModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
@@ -73,4 +154,4 @@ export function HttpLoaderFactory(http: HttpClient) {
   providers: [HttpClient],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { }
